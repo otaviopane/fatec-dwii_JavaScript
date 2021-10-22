1af3afb369d8f2fba79f41fb7426fd62b3340447
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-21 22:37:13 -0300

    Importando modulo de calculadora a funçao soma

---
+++ app.js
@@ -0,0 +1,5 @@
+var somafuncao = require("./calculadora")
+// Fazendo isso eu estou importando o arquivo calculadora.js no arquivo app.js.
+// Para utilizar a função está se recebendo em um variável chamada somafuncao
+
+console.log(somafuncao(10, 20))
---
+++ calculadora.js
@@ -0,0 +1,7 @@
+function soma(a,b) {
+   return a + b
+}
+
+module.exports = soma;
+//  Para conseguir utilizar qualqur coisa que tenha no arquivo calculadora, será necessário exportar. 
+//  Nesse caso a função soma. Assim disponibilizando essa função para acesso em outros arquivos.
