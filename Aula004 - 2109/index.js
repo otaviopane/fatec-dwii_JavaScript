4199cf6abff17526bab1b3de4ff72104f412600b
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-22 00:50:46 -0300

    FInalizado a aula 004 do dia 21/09 de DWII, criado rota simples com string para resolver erro

--- Aula004 - 2109/index.js
+++ Aula004 - 2109/index.js
@@ -1,5 +1,11 @@
 const express = require("express") // importando o express
 const app = express() // iniciando o express e passando para a variavel app
+
+app.get("/", function(requisicao, resposta) { // forma mais simples de criar uma rota
+   resposta.send("Bem Vindo ao Express");
+}); // Toda rota criado precisa devolver uma resposta
+// Nesse caso em formato de texto. Mas pode ser uma página HTML, um arquivo, etc.
+
 app.listen(3000, function(erro) {
    if (erro) {
       console.log('Ocorreu um erro');
