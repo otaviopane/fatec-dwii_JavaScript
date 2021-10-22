3e31016b0a16d1d1ae6649a07b4b4dac0b5f61dd
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-21 23:23:36 -0300

    Importando modulo de calculadora com multiplas funçoes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var somafuncao = require("./calculadora")
+var calculadora = require("./calculadora")
 // Fazendo isso eu estou importando o arquivo calculadora.js no arquivo app.js.
 // Para utilizar a função está se recebendo em um variável chamada somafuncao
 
-console.log(somafuncao(10, 20))
+console.log(calculadora.soma(20, 40))
+console.log(calculadora.sub(10, 20))
+console.log(calculadora.mult(5, 15))
+console.log(calculadora.div(400, 20))
--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,24 @@ function soma(a,b) {
    return a + b
 }
 
-module.exports = soma;
-//  Para conseguir utilizar qualqur coisa que tenha no arquivo calculadora, será necessário exportar. 
-//  Nesse caso a função soma. Assim disponibilizando essa função para acesso em outros arquivos.
+function mult(a, b) {
+   return a * b
+}
+
+function sub(a, b) {
+   return a - b
+}
+
+function div(a, b) {
+   return a / b
+}
+
+
+module.exports = {
+   soma,
+   mult,
+   sub,
+   div
+};
+//  Para conseguir utilizar qualquer coisa que tenha no arquivo calculadora, será necessário exportar. 
+//  Nesse caso a função soma, mult, sub e div. Assim disponibilizando essas funções para acesso em outros arquivos.
