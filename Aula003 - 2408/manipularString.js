c249e8feded5a2b31ad7fe687d1159d4ad5e57ee
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-21 21:30:11 -0300

    Feito aula 24/08 de DWII

---
+++ Aula003 - 2408/manipularString.js
@@ -0,0 +1,20 @@
+// C L A U D I O   L I  M A
+// 0 1 2 3 4 5 6 7 8 9 10 11
+
+
+// length
+document.write('Claudio Lima'.length) // Retorna 12
+// charAt()
+document.write('Claudio Lima'.charAt(8)) // Retorna 'L'
+// indexOf()
+var nome = 'Claudio Lima'
+document.write(nome.indexOf('a')) // Retorna 2
+// replace()
+var nome = 'Claudio Lima'
+document.write(nome.replace('Lima', 'Silva')) // Retorna 'Claudio Silva'
+// substr()
+var nome = 'Claudio Lima'
+document.write(nome.substr(6, 4)) // Retorna 'o Li'
+// toLowerCase()
+var nome = 'Claudio Lima'
+document.write(nome.toLowerCase()) // Retorna 'claudio lima'
